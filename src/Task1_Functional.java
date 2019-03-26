e1d40b022977e040dde1aacf719c96056f2118a4
Qais Patankar <qaisjp@gmail.com>
2019-03-26 20:04:56 +0000

    Add some skeleton tests

--- src/Task1_Functional.java
+++ src/Task1_Functional.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import st.Parser;
@@ -18,5 +19,53 @@ public class Task1_Functional {
 		parser.add("output", "o", Parser.STRING);
 		parser.parse("--output=output.txt");
 		assertEquals(parser.getString("o"), "output.txt");
+	}
+
+	@Test
+	@Ignore
+	public void shortInt() {
+
+	}
+
+	@Test
+	@Ignore
+	public void longInt() {
+
+	}
+
+	@Test
+	@Ignore
+	public void shortBool() {
+
+	}
+
+	@Test
+	@Ignore
+	public void longBool() {
+
+	}
+
+	@Test
+	@Ignore
+	public void shortStr() {
+
+	}
+
+	@Test
+	@Ignore
+	public void longStr() {
+
+	}
+
+	@Test
+	@Ignore
+	public void shortChar() {
+
+	}
+
+	@Test
+	@Ignore
+	public void longChar() {
+
 	}
 }
