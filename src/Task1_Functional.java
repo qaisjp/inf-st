6b1bf8d4d288a510ad25c7c12656826890b6b538
Qais Patankar <qaisjp@gmail.com>
2019-03-26 20:01:01 +0000

    Add example code to Task1

--- src/Task1_Functional.java
+++ src/Task1_Functional.java
@@ -1,12 +1,22 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import st.Parser;
 
-import org.junit.Before;
-import static org.junit.Assert.assertEquals;
-
 public class Task1_Functional {
+	private Parser parser;
 
+	@Before
+	public void setup() {
+		parser = new Parser();
+	}
+
+	@Test
+	public void example() {
+		parser.add("output", "o", Parser.STRING);
+		parser.parse("--output=output.txt");
+		assertEquals(parser.getString("o"), "output.txt");
+	}
 }
